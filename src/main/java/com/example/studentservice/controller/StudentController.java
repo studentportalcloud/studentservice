c6a8ee402c000fb6086acf13d32fdb8c2e86753a
Sagar Mamdapure <sagarmamdapure@gmail.com>
2021-06-04 00:58:08 +0530

    changed endpoint (#1)

--- src/main/java/com/example/studentservice/controller/StudentController.java
+++ src/main/java/com/example/studentservice/controller/StudentController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StudentController {
 
-    @GetMapping("/hello-world")
+    @GetMapping
     public String sayHello() {
         return "Hello World";
     }
