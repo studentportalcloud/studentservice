b7c0fa168d93ac2dfc77036f5ed5534a8dee4b68
Sagar Mamdapure <sagarmamdapure@gmail.com>
2021-06-04 01:10:29 +0530

    disabled cors policy

--- src/main/java/com/example/studentservice/controller/StudentController.java
+++ src/main/java/com/example/studentservice/controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.studentservice.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class StudentController {
 
     @GetMapping
+    @CrossOrigin
     public String sayHello() {
         return "Hello World";
     }
